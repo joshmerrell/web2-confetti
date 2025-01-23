7d1b1cd61d370c9e1a5a538dc35e3d60664dd154
Joshua Merrell <84419054+joshmerrell@users.noreply.github.com>
2025-01-23 19:35:05 +0000

    Add confetti stuff

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("JavaScript laoded");
+// Source for Confetti library
+// https://confettijs.org/confetti.min.js
+// ✅ Connect Confetti source code to `<head>` of `index.html`
+//⚠️ What is loaded first? Confetti or your scripts?
+
+// Create variables, instantiate `new` instance of `Confetti`
+// Where to confetti? Target `id` of element
+// Understand context of uses of "confetti"
+let confetti = new Confetti("confetti");
+
+// Optionally modify given parameters
+confetti.setCount(100);
+confetti.setSize(10);
+confetti.setPower(100);
+confetti.destroyTarget(false);
